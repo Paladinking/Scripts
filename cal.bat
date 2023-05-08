691b79785fedb27d74fdcd4b6dfe82cf5cfdf189
Axel <axel.hammarberg@gmail.com>
2023-05-08 11:54:46 +0200

    Added windwos shebang

--- cal.bat
+++ cal.bat
@@ -1,14 +1,116 @@
-@echo off
-setlocal
+@echo off & python -x %~f0 %* & exit /B
+import math
+import sys
+from math import sin, cos, pi, sqrt, log
 
-if [%1] EQU [] (
-	goto :end
-)
+old_bin = bin
+old_hex = hex
+old_log = log
 
-pushd C:\Users\axelh\Scripts
+def ln(x):
+    return old_log(x)
 
-python cal.py %*
+def log(x):
+    return old_log(x, 10)
 
-popd
-:end
-endlocal
+def logn(x, n):
+    return old_log(x, n)
+
+def bin(v, n = None):
+    val = old_bin(v)[2:]
+    if n is None:
+        return val
+    return "0" * (n - len(val)) + val 
+
+def binf(v, n = None):
+    return "0b" + bin(v, n)
+
+def hex(v, n = None):
+    val = old_hex(v)[2:]
+    if n is None:
+        return val
+    return "0" * (n - len(val)) + val
+
+def hexf(v, n = None):
+    return "0x" + hex(v, n)
+
+def reduce(f, first, it):
+    for val in it:
+        first = f(first, val)
+    return first
+    
+def foreach(f, it):
+    for val in it:
+        f(val)
+
+fold = reduce
+
+def reduce_through(f, val, it):
+    for v in it:
+        f(val, v)
+    return val
+
+fold_through = reduce_through
+
+def index(i):
+    return lambda x : x[i]
+
+def call(f, *args, pos = 0):
+    first = args[0:pos]
+    second = args[pos:]
+    return lambda x : f(*first, x, *second)
+
+old_int = int
+
+def int(s, base = None):
+    if not isinstance(s, str):
+        return old_int(s)
+    if not base is None:
+        return old_int(s, base)
+    if len(s) > 2 and s[0] == '0':
+        if s[1] == 'x' or s[1] == 'X':
+            return old_int(s, 16)
+        elif s[1] == 'b' or s[1] == 'B':
+            return old_int(s, 2)
+        elif s[1] == 'o' or s[1] == 'O':
+            return old_int(s, 8)
+    return old_int(s, 10) 
+
+variables = dict()
+
+def store(name):
+    def f(x):
+        variables[name] = x
+    return f
+
+def get(name):
+    return variables[name]
+    
+def do(*data, ret=0):
+    return data[ret]
+
+def main():
+    if len(sys.argv) < 2:
+        return
+    arg = ""
+    for val in sys.argv[1:]:
+        arg += val + " "
+    args = arg.split("!")
+    first = args[0].split(";")
+    res = eval(first[0])
+    for arg in first[1:]:
+        _ = res
+        res = eval(arg)
+
+    for step in args[1:]:
+        parts = step.split(";")
+        _ = res
+        res = eval(parts[0])(res)
+        for part in parts[1:]:
+            _ = res
+            res = eval(part)
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
--- ls.bat
+++ ls.bat
@@ -1,2 +1,19 @@
-@echo off
-python %~dp0\ls.py %*
+@echo off & python -x %~f0 %* & exit /B
+import sys
+import subprocess
+
+if __name__ == "__main__":
+    res = subprocess.run(['dir', '/w', *sys.argv[1:]], shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    stdout = res.stdout.decode('cp437')
+    if stdout:        
+        if stdout.split('\n')[0].startswith(' Volume in drive'):
+            lst = stdout.split('\n')
+            lst = lst[5:-3]
+            stdout = ''
+            for line in lst:
+                stdout += line + '\n'
+        print(stdout, end='')
+    stderr = res.stderr.decode('cp437')
+    if stderr:
+        print(stderr, end='')
+
