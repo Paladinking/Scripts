160c40e3a9f0247ce21b23e1c337623c149eec3c
Axel <axel.hammarberg@gmail.com>
2023-07-28 21:14:12 +0200

    Updated cmdrc

--- cmdrc.bat
+++ cmdrc.bat
@@ -1,8 +1,6 @@
 @echo off
 :: Regedit HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Command Processor add key AutoRun pointing to this file
 doskey notepad++="C:\Program files (x86)\Notepad++\notepad++.exe" $*
-doskey clear=cls
-doskey cat=type $*
-doskey mv=move $*
-doskey cp=copy $*
-doskey rm=del $*
+doskey ls=ls --color $*
+doskey kill=taskkill /IM $*
+doskey fkill=taskkill /F /IM $*
