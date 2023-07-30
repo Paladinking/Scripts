0730875839ad3e7be63d9a750b056362757a791a
Axel Hammarberg <axeha009@student.liu.se>
2023-07-30 14:46:34 +0200

    Updated structure

--- workdir/build.bat
+++ workdir/build.bat
@@ -1,35 +1,35 @@
 @setlocal
-@call path-add nasm >nul
 echo NASM
-nasm -fwin64 short.asm -o short.obj && echo  short.obj
-nasm -fwin64 stdasm.asm -o stdasm.obj && echo  stdasm.obj
-nasm -fwin64 args.asm -o args.obj && echo  args.obj
-nasm -fwin64 chkstk.asm -o chkstk.a && echo  chkstk.a
+@call path-add nasm >nul
+nasm -fwin64 src/short.asm -o build/short.obj && echo  build/short.obj
+nasm -fwin64 src/stdasm.asm -o build/stdasm.obj && echo  build/stdasm.obj
+nasm -fwin64 src/args.asm -o build/args.obj && echo  build/args.obj
+nasm -fwin64 src/chkstk.asm -o build/chkstk.a && echo  build/chkstk.a
 @endlocal
 @setlocal
-@call path-add msvc >nul
 echo MSVC
-cl short.obj stdasm.obj args.obj Kernel32.lib /link /NODEFAULTLIB /entry:main > build_out.txt 2>&1
+@call path-add msvc >nul
+cl build/short.obj build/stdasm.obj build/args.obj Kernel32.lib /Fe:bin/short.exe /link /NODEFAULTLIB /entry:main > build_out.txt 2>&1
 if [%errorlevel%] NEQ [0] (
 	type build_out.txt
 ) else (
-	echo  short.exe
+	echo  bin/short.exe
 )
-cl stdasm.obj args.obj Kernel32.lib chkstk.obj tests.c /GS- /Fe:tests.exe /link /NODEFAULTLIB /entry:main > build_out.txt 2>&1
+cl build/stdasm.obj build/args.obj Kernel32.lib chkstk.obj src/stdasm_test.c /GS- /Fe:test\stdasm_test.exe /link /NODEFAULTLIB /entry:main > build_out.txt 2>&1
 if [%errorlevel%] NEQ [0] (
 	type build_out.txt
 ) else (
-	echo  tests.exe
-	tests.exe
+	echo  test/stdasm_test.exe
+	test\stdasm_test.exe
 )
 if exist build_out.txt (
 	del build_out.txt
 )
 @endlocal
 @setlocal
-@call path-add mingw >nul
 echo MINGW
-gcc tests.c stdasm.obj chkstk.a -lKernel32 --entry=entry_main -nostdlib -o tests-gcc.exe && echo  tests-gcc.exe && tests-gcc.exe
+@call path-add mingw >nul
+gcc src/stdasm_test.c build/stdasm.obj build/chkstk.a -lKernel32 --entry=entry_main -nostdlib -o test/stdasm_test-gcc.exe && echo  test/stdasm_test-gcc.exe && test\stdasm_test-gcc.exe
 @endlocal
 
 
