1a3aaad14ad28614547eea198401adeed3cb2d20
Axel <detektd.name@gmail.com>
2023-08-20 09:31:21 +0200

    Changes to path-add

--- path-add.bat
+++ path-add.bat
@@ -3,12 +3,24 @@
 if "%~1" EQU "" (
 	exit /b 2
 )
-:loop
-echo ^^^|RESET| findstr /i "|%~1\>" >nul
-if [%errorlevel%] EQU [0] (
-	path %~dp0;C:\Windows\System32;C:\Windows;
-	set "VSCMD_VER="
+
+if not defined SYSTEMROOT (
+	set SYSTEMROOT=C:\Windows
 )
+
+:loop
+echo ^^^|CLEAN| %SYSTEMROOT%\System32\findstr /i "|%~1\>" >nul
+if [%errorlevel%] EQU [0] (
+	path %~dp0;%SYSTEMROOT%\System32;%SYSTEMROOT%;
+	echo Cleaned path
+)
+
+echo ^^^|RESET| %SYSTEMROOT%\System32\findstr /i "|%~1\>" >nul
+if [%errorlevel%] EQU [0] (
+	call cleanenv.bat
+	echo Reset environment
+)
+
 
 for /f "delims=<" %%i in ('findstr /i "<%~1\>" %~dp0paths.txt') do (
 	call path-add %%i
@@ -17,7 +29,7 @@ for /f "delims=>" %%i in ('findstr /i ">%~1\>" %~dp0paths.txt') do (
 	call %%i
 )
 for /f "delims=|" %%i in ('findstr /i "|%~1\>" %~dp0paths.txt') do (
-	call :setpath %%i
+	call :add_path %%i
 )
 shift /1
 if "%~1" EQU "" (
