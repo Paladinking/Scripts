8f26e32075b50b514036411dfcd175c6a084fcca
Axel <detektd.name@gmail.com>
2023-09-27 14:50:20 +0200

    Removed installation

--- cmdrc.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Regedit HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Command Processor add key AutoRun pointing to this file
-doskey notepad++="C:\Program files (x86)\Notepad++\notepad++.exe" $*
-doskey ls=ls --color $*
-doskey kill=taskkill /IM $*
-doskey fkill=taskkill /F /IM $*
-doskey path-set=path-add RESET $*
-doskey poetry=if "$1" EQU "enter" (for /f "tokens=*" %%i in ('poetry.exe env info --path') do @(%%i\Scripts\activate.bat)) else if "$1" EQU "leave" (for /f "tokens=*" %%i in ('poetry.exe env info --path') do @(%%i\Scripts\deactivate.bat)) else poetry.exe $*
--- pathc.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-pathc.exe %* > nul
-if [%errorlevel%] EQU [1] (
-	exit /b 1
-)
-for /f tokens^=^*^ eol^= %%i in ('pathc.exe %*') do (
-	path %%i
-)
-exit /b 0
