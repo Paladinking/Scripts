9d16c02baf2ded697a11569590634e7e8c156b18
Axel <axel.hammarberg@gmail.com>
2024-06-03 22:41:28 +0200

    Allowed _printf and _wprintf to write to pipe / file

--- workdir/src/printf.c
+++ workdir/src/printf.c
@@ -14,6 +14,23 @@ extern int _vsnwprintf(wchar_t *buffer, size_t count, const wchar_t *format,
 }
 #endif
 
+void outputa(HANDLE out, char* data, size_t size) {
+    DWORD type = GetFileType(out);
+    if (type == FILE_TYPE_CHAR) {
+        // TODO: Convert to UTF-16 and use WriteConsoleW??
+        WriteConsoleA(out, data, size, NULL, NULL);
+    } else {
+        DWORD written = 0;
+        while (written < size) {
+            DWORD w;
+            if (!WriteFile(out, data + written, size - written, &w, NULL)) {
+                break;
+            }
+            written += w;
+        }
+    }
+}
+
 int _printf_h(HANDLE dest, const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
@@ -32,11 +49,42 @@ int _printf_h(HANDLE dest, const char *fmt, ...) {
             return -1;
         }
         _vsnprintf(buf, count, fmt, args);
-        WriteConsoleA(dest, buf, count, NULL, NULL);
+        outputa(dest, buf, count);
         HeapFree(heap, 0, buf);
     }
     va_end(args);
     return count;
+}
+
+void outputw(HANDLE out, wchar_t* data, size_t size) {
+    DWORD type = GetFileType(out);
+    if (type == FILE_TYPE_CHAR) {
+        WriteConsoleW(out, data, size, NULL, NULL);
+    } else {
+        char buf[1024];
+        int outsize = WideCharToMultiByte(CP_UTF8, 0, data, size, NULL, 0, NULL, NULL);
+        char* output;
+        if (outsize <= 1024) {
+            output = buf;
+        } else {
+            output = (char*)HeapAlloc(GetProcessHeap(), 0, outsize);
+            if (output == NULL) {
+                return;
+            }
+        }
+        WideCharToMultiByte(CP_UTF8, 0, data, size, output, outsize, NULL, NULL);
+        DWORD written = 0;
+        while (written < outsize) {
+            DWORD w;
+            if (!WriteFile(out, output + written, outsize - written, &w, NULL)) {
+                break;
+            }
+            written += w;
+        }
+        if (outsize > 1024) {
+            HeapFree(GetProcessHeap(), 0, output);
+        }
+    }
 }
 
 int _wprintf_h(HANDLE dest, const wchar_t *fmt, ...) {
@@ -49,7 +97,7 @@ int _wprintf_h(HANDLE dest, const wchar_t *fmt, ...) {
     if (count < 512) { // Note: not inclusive for NULL terminator
         wchar_t buf[512];
         _vsnwprintf(buf, count, fmt, args);
-        WriteConsoleW(dest, buf, count, NULL, NULL);
+        outputw(dest, buf, count);
     } else {
         HANDLE heap = GetProcessHeap();
         wchar_t *buf = (wchar_t *)HeapAlloc(heap, 0, count * sizeof(wchar_t));
@@ -57,7 +105,7 @@ int _wprintf_h(HANDLE dest, const wchar_t *fmt, ...) {
             return -1;
         }
         _vsnwprintf(buf, count, fmt, args);
-        WriteConsoleW(dest, buf, count, NULL, NULL);
+        outputw(dest, buf, count);
         HeapFree(heap, 0, buf);
     }
     va_end(args);
