df77f8f8b61229aab5eaa49c2c6ceec99ccdeb64
Axel <axel.hammarberg@gmail.com>
2024-10-17 23:31:36 +0200

    More usage of dynamic_string

--- workdir/src/path-add.c
+++ workdir/src/path-add.c
@@ -3,11 +3,12 @@
 #define main entry_main
 #endif
 #include "args.h"
+#include "dynamic_string.h"
 #include "path_utils.h"
 #include "printf.h"
 #include "string_conv.h"
-#include <wchar.h>
 #include <tlhelp32.h>
+#include <wchar.h>
 #include <windows.h>
 
 DWORD GetParentProcessId() {
@@ -101,7 +102,7 @@ LPSTR read_paths_file(LPCWSTR name, LPCWSTR ext, LPCWSTR binpath,
     return buffer;
 }
 
-WideBuffer add_buffer = {NULL, 0, 0};
+WString add_buffer = {NULL, 0, 0};
 
 EnvBuffer environment = {NULL, 0, 0};
 WCHAR gBinpath[260];
@@ -110,7 +111,6 @@ OpStatus parse_env_file(LPSTR file, DWORD count, LPSTR file_name,
                         DWORD file_name_len) {
     HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
     HANDLE err = GetStdHandle(STD_ERROR_HANDLE);
-    HANDLE heap = GetProcessHeap();
     LPSTR line = file;
     OpStatus res = OP_NO_CHANGE;
     for (unsigned i = 0; i < count; ++i) {
@@ -120,9 +120,9 @@ OpStatus parse_env_file(LPSTR file, DWORD count, LPSTR file_name,
         LPSTR line_name = NULL;
         if (line[0] == '!' && line[1] == '!') {
             DWORD len = strlen(line + 2);
-            NarrowStringToWide(line + 2, len, &add_buffer);
-            WideBuffer *var = read_envvar(add_buffer.ptr, &environment);
-            if (var != NULL && var->size > 0) {
+            WString_from_utf8_bytes(&add_buffer, line + 2, len);
+            WString *var = read_envvar(add_buffer.buffer, &environment);
+            if (var != NULL && var->length > 0) {
                 return res;
             }
         }
@@ -138,14 +138,14 @@ OpStatus parse_env_file(LPSTR file, DWORD count, LPSTR file_name,
             (line_name = strchr(line, '<')) != NULL) {
             DWORD len = line_name - line;
             DWORD name_len = strlen(line_name + 1);
-            NarrowStringToWide(line_name + 1, name_len, &add_buffer);
-            WideBuffer *var = read_envvar(add_buffer.ptr, &environment);
+            WString_from_utf8_bytes(&add_buffer, line_name + 1, name_len);
+            WString *var = read_envvar(add_buffer.buffer, &environment);
             if (var == NULL) {
                 _wprintf_h(err, L"Invalid environment variable %.*s\n",
-                           add_buffer.size, add_buffer.ptr);
+                           add_buffer.length, add_buffer.buffer);
             } else if (*line_name == '|') {
-                NarrowStringToWide(line, len, &add_buffer);
-                OpStatus status = path_add(add_buffer.ptr, var, TRUE, FALSE);
+                WString_from_utf8_bytes(&add_buffer, line, len);
+                OpStatus status = path_add(add_buffer.buffer, var, TRUE, FALSE);
                 if (status > OP_NO_CHANGE) {
                     _wprintf_h(err, L"Error while parsing file %.*S: %S\n",
                                file_name_len, file_name, line);
@@ -156,31 +156,23 @@ OpStatus parse_env_file(LPSTR file, DWORD count, LPSTR file_name,
                     res = OP_SUCCESS;
                 }
             } else if (*line_name == '>') {
-                NarrowStringToWide(line, len, &add_buffer);
-                WideBuffer temp = add_buffer;
+                WString_from_utf8_bytes(&add_buffer, line, len);
+                WString temp = add_buffer;
                 add_buffer = *var;
                 *var = temp;
                 res = OP_SUCCESS;
             } else {
-                NarrowStringToWide(line, len, &add_buffer);
-                WideBuffer *to_save = read_envvar(add_buffer.ptr, &environment);
+                WString_from_utf8_bytes(&add_buffer, line, len);
+                WString *to_save = read_envvar(add_buffer.buffer, &environment);
                 if (to_save == NULL) {
                     _wprintf_h(err, L"Invalid environment variable %.*s\n",
-                               add_buffer.size, add_buffer.ptr);
+                               add_buffer.length, add_buffer.buffer);
                 } else {
-                    if (var->capacity < to_save->size + 1) {
-                        LPWSTR buf = HeapAlloc(
-                            heap, 0, (to_save->size + 1) * sizeof(WCHAR));
-                        if (buf == NULL) {
-                            return OP_OUT_OF_MEMORY;
-                        }
-                        var->capacity = to_save->size + 1;
-                        var->size = to_save->size;
-                        HeapFree(heap, 0, var->ptr);
-                        var->ptr = buf;
+                    WString_clear(var);
+                    if (!WString_append_count(var, to_save->buffer,
+                                              to_save->length)) {
+                        return OP_OUT_OF_MEMORY;
                     }
-                    memcpy(var->ptr, to_save->ptr,
-                           (to_save->size + 1) * sizeof(WCHAR));
                     res = OP_SUCCESS;
                 }
             }
@@ -211,9 +203,9 @@ OpStatus add_paths(LPSTR paths, DWORD count, LPSTR name, LPCWSTR binpath) {
                 ++line;
                 len -= 2;
             }
-            NarrowStringToWide(line, len, &add_buffer);
+            WString_from_utf8_bytes(&add_buffer, line, len);
             OpStatus status =
-                path_add(add_buffer.ptr, &environment.vars[0].val, TRUE, FALSE);
+                path_add(add_buffer.buffer, &environment.vars[0].val, TRUE, FALSE);
             if (status > OP_NO_CHANGE) {
                 _printf_h(err, "Failed adding %.*s to Path\n", len, line);
                 if (status != OP_INVALID_PATH) {
@@ -238,33 +230,33 @@ OpStatus add_paths(LPSTR paths, DWORD count, LPSTR name, LPCWSTR binpath) {
         } else if ((line_name = strchr(line, '>')) != NULL &&
                    _stricmp(line_name + 1, name) == 0) {
             DWORD len = line_name - line;
-            NarrowStringToWide(line, len, &add_buffer);
-            LPWSTR ext = add_buffer.ptr + add_buffer.size;
-            while (ext > add_buffer.ptr && *ext != L'.') {
+            WString_from_utf8_bytes(&add_buffer, line, len);
+            LPWSTR ext = add_buffer.buffer + add_buffer.length;
+            while (ext > add_buffer.buffer && *ext != L'.') {
                 --ext;
             }
-            if (ext == add_buffer.ptr) {
+            if (ext == add_buffer.buffer) {
                 ext = NULL;
             } else {
                 *ext = L'\0';
                 ext += 1;
             }
             DWORD lines;
-            LPSTR file = read_paths_file(add_buffer.ptr, ext, gBinpath, &lines);
+            LPSTR file = read_paths_file(add_buffer.buffer, ext, gBinpath, &lines);
             if (ext != NULL) {
                 *(ext - 1) = L'.';
             }
             if (file == NULL) {
-                _wprintf_h(err, L"Could not open %.*s\n", add_buffer.size,
-                           add_buffer.ptr);
+                _wprintf_h(err, L"Could not open %.*s\n", add_buffer.length,
+                           add_buffer.buffer);
             } else {
                 OpStatus status = parse_env_file(file, lines, line, len);
+                HeapFree(heap, 0, file);
                 if (status == OP_SUCCESS) {
                     res = OP_SUCCESS;
                 } else if (status > OP_INVALID_PATH) {
                     return status;
                 }
-                HeapFree(heap, 0, file);
             }
             line = line_name + name_len + 2;
         } else {
@@ -273,30 +265,36 @@ OpStatus add_paths(LPSTR paths, DWORD count, LPSTR name, LPCWSTR binpath) {
     }
     return res;
 }
-const char* help_message =
+const char *help_message =
     "usage: path-add [NAME]...\n"
     "Goes through paths.txt and adds all matching lines.\n"
     "The paths.txt file should be located next to the path-add binary.\n"
     "\n"
     "Lines in paths.txt can have 3 different formats:\n"
     " 1. {PATH}|{NAME}\n"
-    "  If {NAME} matches an input argument, {PATH} will be added to PATH environment variable.\n"
+    "  If {NAME} matches an input argument, {PATH} will be added to PATH "
+    "environment variable.\n"
     "  Note that | is a literal and should be present in the file.\n"
     " 2. {CHILD}<{NAME}\n"
-    "  If {NAME} matches an input argument, add {CHILD} next in argument list.\n"
-    "  This can (but does not have to) cause infinite recursion for bad input, causing the program to run out of memory.\n"
+    "  If {NAME} matches an input argument, add {CHILD} next in argument "
+    "list.\n"
+    "  This can (but does not have to) cause infinite recursion for bad input, "
+    "causing the program to run out of memory.\n"
     " 3. {FILE}>{NAME}\n"
-    "  If {NAME} matches an input argument, parse {FILE} for environment variables.\n"
+    "  If {NAME} matches an input argument, parse {FILE} for environment "
+    "variables.\n"
     "\n"
     "All other lines are ignored.\n"
     "\n"
-    "Lines in an environment variable file given by format 3 in paths.txt can have 5 different formats:\n"
+    "Lines in an environment variable file given by format 3 in paths.txt can "
+    "have 5 different formats:\n"
     " 1. !!{VAR}\n"
     "  Stop parsing file if {VAR} is defined.\n"
     " 2. **{TEXT}\n"
     "  {TEXT} is printed to the console.\n"
     " 3. {PATH}|{VAR}\n"
-    "  {PATH} will be prepended to the environment variable {VAR}, separated by semi-colons.\n"
+    "  {PATH} will be prepended to the environment variable {VAR}, separated "
+    "by semi-colons.\n"
     " 4. {VALUE}>{VAR}\n"
     "  The environment variable {VAR} will be set to {VALUE}.\n"
     "  Just >{VAR} is allowed and will delete the environment variable {VAR}.\n"
@@ -311,6 +309,8 @@ int main() {
     LPWSTR args = GetCommandLine();
     int argc;
     int status = 0;
+
+    WString_create(&add_buffer);
     LPWSTR *argv = parse_command_line(args, &argc);
     if (argc < 2) {
         status = 1;
@@ -380,15 +380,15 @@ int main() {
     }
     DWORD id = GetParentProcessId();
     if (id == 0) {
-        WriteConsole(err, L"Could not get parent process\n", 29, NULL, NULL);
+        WriteConsoleW(err, L"Could not get parent process\n", 29, NULL, NULL);
         status = 4;
         goto end;
     }
 
     for (DWORD i = 0; i < environment.size; ++i) {
         LPCWSTR name = environment.vars[i].name;
-        LPCWSTR val = environment.vars[i].val.ptr;
-        if (SetEnvironmentVariable(name, val)) {
+        LPCWSTR val = environment.vars[i].val.buffer;
+        if (SetEnvironmentVariableW(name, val)) {
             if (SetProcessEnvironmentVariable(name, val, id) == 0) {
                 continue;
             }
@@ -397,7 +397,7 @@ int main() {
     }
 end:
     free_env(&environment);
-    HeapFree(heap, 0, add_buffer.ptr);
+    WString_free(&add_buffer);
     FlushFileBuffers(out);
     FlushFileBuffers(err);
     HeapFree(heap, 0, expanded);
