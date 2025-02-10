a25b6fcc8744335cc55d49096c86b688c1cc6063
Axel <detektd.name@gmail.com>
2025-02-10 14:12:39 +0100

    Better EastAsianWidth script

--- workdir/EastAsianWidth.py
+++ workdir/EastAsianWidth.py
@@ -1,22 +1,35 @@
 from typing import List, Tuple
+import sys
 
 # Find smallest element where upper >= c
-def get_part(ranges: List[Tuple[int, int]], known: bool) -> str:
-    #print("//", ranges, known)
+def get_part(ranges: List[Tuple[int, int]], known: bool, depth = 1) -> str:
+    tab = "\t" * depth
     if len(ranges) == 1:
         if known:
-            return f"\treturn c >= {ranges[0][0]};"
-        return f"\treturn c <= {ranges[0][1]} && c >= {ranges[0][0]};"
+            return tab + f"return c >= {ranges[0][0]};"
+        return tab + f"return c <= {ranges[0][1]} && c >= {ranges[0][0]};"
     if len(ranges) == 2:
         if known:
-            return f"if (c <= {ranges[0][1]}) {{\n\treturn c >= {ranges[0][0]};\n}} else {{\n\treturn c >= {ranges[1][0]};\n}}"
-        part = get_part(ranges[1:], False)
-        return f"if (c <= {ranges[0][1]}) {{\n\treturn c >= {ranges[0][0]};\n}} else {{\n{part}\n}}"
+            return tab + f"if (c <= {ranges[0][1]}) {{\n" + \
+                   tab + f"\treturn c >= {ranges[0][0]};\n" + \
+                   tab + f"}} else {{\n" + \
+                   tab + f"\treturn c >= {ranges[1][0]};\n" + \
+                   tab + f"}}"
+        part = get_part(ranges[1:], False, depth + 1)
+        return tab + f"if (c <= {ranges[0][1]}) {{\n" + \
+               tab + f"\treturn c >= {ranges[0][0]};\n" + \
+               tab + f"}} else {{\n" + \
+                     f"{part}\n" + \
+               tab + f"}}"
     split = len(ranges) // 2
     p1 = ranges[:split + 1]
     r = ranges[split]
     p2 = ranges[split + 1:]
-    return f"if (c <= {r[1]}) {{\n{get_part(p1, True)}\n}} else {{\n{get_part(p2, known)}\n}}"
+    return tab + f"if (c <= {r[1]}) {{\n" + \
+                 f"{get_part(p1, True, depth + 1)}\n" + \
+           tab + f"}} else {{\n" + \
+                 f"{get_part(p2, known, depth + 1)}\n" + \
+           tab + f"}}"
 
 def main() -> None:
     with open('EastAsianWidth.txt', 'r') as file:
@@ -36,10 +49,20 @@ def main() -> None:
         wide = width not in {'N', 'Na', 'H', 'A'}
         if wide:
             ranges.append(urange)
-    print("//File generated py EastAsianWidth.py")
-    print("#include <windows.h>")
+    if not ranges:
+        print("No data found", file=sys.stderr)
+        exit(1)
+    ranges = sorted(ranges)
+
+    print("// File generated py EastAsianWidth.py")
+    print("#include \"unicode_width.h\"\n")
     print("BOOL is_wide(UINT32 c) {")
-    print(get_part(ranges, False))
+
+    print(f'    if (c < {ranges[0][0]}) {{')
+    print(f'        return FALSE;')
+    print(f'    }}')
+
+    print(get_part(ranges, False).replace('\t', '    '))
     print("}")
 
 if __name__ == '__main__':
