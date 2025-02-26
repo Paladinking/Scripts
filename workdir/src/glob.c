d5192e9603de9a098d9ce64bf1ccce9b3b856904
Axel <detektd.name@gmail.com>
2025-02-26 20:35:21 +0100

    Glob

--- workdir/src/glob.c
+++ workdir/src/glob.c
@@ -174,6 +174,261 @@ DWORD get_file_attrs(const wchar_t* path) {
     return attrs;
 }
 
+bool matches(const wchar_t* pattern, const wchar_t* str) {
+    // Remove . and ..
+    if (str[0] == L'.' && (str[1] == L'\0' || (str[1] == L'.' && str[2] == L'\0'))) {
+        return false;
+    }
+    uint32_t p_ix = 0;
+    uint32_t s_ix = 0;
+    int64_t glob_ix = -1;
+    while (pattern[p_ix] != L'\0') {
+        wchar_t c = pattern[p_ix];
+        if (c == L'*') {
+            if (str[s_ix] == L'\0') {
+                while (pattern[p_ix] != L'\0') {
+                    if (pattern[p_ix] != L'*') {
+                        return false;
+                    }
+                    ++p_ix;
+                }
+                return true;
+            } else if (str[s_ix] == L'.' && s_ix == 0) {
+                return false;
+            } else if (pattern[p_ix + 1] == L'?' || towlower(str[s_ix]) == towlower(pattern[p_ix + 1])) {
+                glob_ix = p_ix;
+                p_ix += 2;
+                ++s_ix;
+            } else {
+                ++s_ix;
+            }
+            continue;
+        } else if (c == L'?') {
+            if (str[s_ix] == L'.' && s_ix == 0) {
+                return false;
+            }
+            ++s_ix;
+            ++p_ix;
+        } else {
+            if (towlower(str[s_ix]) != towlower(c)) {
+                if (glob_ix < 0) {
+                    return false;
+                }
+                p_ix = glob_ix;
+                continue;
+            }
+            ++s_ix;
+            ++p_ix;
+        }
+    }
+    return str[s_ix] == L'\0';
+}
+
+
+bool Glob_begin(const wchar_t* pattern, GlobCtx* ctx) {
+    ctx->handle = INVALID_HANDLE_VALUE;
+    ctx->stack_size = 0;
+    ctx->stack = NULL;
+    wchar_t* s = Mem_alloc((wcslen(pattern) + 1) * sizeof(wchar_t));
+    if (s == NULL) {
+        return false;
+    }
+
+    uint32_t offset = 0;
+
+    if (pattern[0] == L'/' && ((pattern[1] >= 'a' && pattern[1] <= 'z') || 
+        (pattern[1] >= 'A' && pattern[1] <= 'Z')) && 
+        (pattern[2] == L'\0' || pattern[2] == L'/' || pattern[2] == L'\\')) {
+        s[0] = pattern[1];
+        s[1] = L':';
+        offset = 2;
+    }
+
+    bool has_glob = false;
+    for (; pattern[offset] != L'\0'; ++offset) {
+        if (pattern[offset] == L'/') {
+            s[offset] = L'\\';
+        } else {
+            s[offset] = pattern[offset];
+        }
+        if (s[offset] == L'*' || s[offset] == L'?') {
+            has_glob = true;
+        }
+    }
+    s[offset] = L'\0';
+    if (!has_glob) {
+        goto fail;
+    }
+    if (!WString_create_capacity(&ctx->p.path, offset)) {
+        goto fail;
+    }
+
+    ctx->stack_capacity = 4;
+    ctx->stack = Mem_alloc(4 * sizeof(ctx->stack[0]));
+    if (ctx->stack == NULL) {
+        WString_free(&ctx->p.path);
+        goto fail;
+    }
+    ctx->stack_size = 1;
+    ctx->stack[0].pattern_offset = 0;
+    ctx->stack[0].pattern = s;
+    ctx->last_segment = 0;
+
+    return true;
+fail:
+    Mem_free(s);
+    return false;
+}
+
+bool Glob_next(GlobCtx* ctx, Path** path) {
+    // Stack is NULL if init failed, or stack is emtied and freed.
+    if (ctx->stack == NULL) {
+        return false;
+    }
+    Path* p = &ctx->p;
+    WIN32_FIND_DATAW data;
+
+    WString_clear(&p->path);
+    while (ctx->stack_size > 0) {
+        struct _GlobCtxNode n = ctx->stack[ctx->stack_size - 1];
+        if (ctx->handle != INVALID_HANDLE_VALUE) {
+            while (FindNextFileW(ctx->handle, &data)) {
+                if (!matches(n.pattern + ctx->last_segment, data.cFileName)) {
+                    continue;
+                }
+                if (!WString_append_count(&p->path, n.pattern, ctx->last_segment)) {
+                    goto fail;
+                }
+                if (!WString_extend(&p->path, data.cFileName)) {
+                    goto fail;
+                }
+                *path = p;
+                return true;
+            }
+            --ctx->stack_size;
+            Mem_free(n.pattern);
+            ctx->handle = INVALID_HANDLE_VALUE;
+            continue;
+        }
+
+        bool has_glob = false;
+        ctx->last_segment = n.pattern_offset;
+        for (uint32_t ix = n.pattern_offset; true ; ++ix) {
+            if (!has_glob && n.pattern[ix] == L'\\') {
+                ctx->last_segment = ix + 1;
+            } else if (n.pattern[ix] == L'*' || n.pattern[ix] == L'?') {
+                has_glob = true;
+            } else if (has_glob && n.pattern[ix] == L'\\') {
+                uint32_t rem_len = wcslen(n.pattern + ix);
+                n.pattern[ix] = L'\0';
+                HANDLE h = FindFirstFileW(n.pattern, &data);
+                --ctx->stack_size;
+                if (h == INVALID_HANDLE_VALUE) {
+                    Mem_free(n.pattern);
+                    break;
+                }
+                do {
+                    if (!(data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+                        continue;
+                    }
+                    if (!matches(n.pattern + n.pattern_offset, data.cFileName)) {
+                        continue;
+                    }
+                    if (ctx->stack_size == ctx->stack_capacity) {
+                        uint32_t cap = 2 * ctx->stack_size;
+                        struct _GlobCtxNode* stack = 
+                            Mem_realloc(ctx->stack, 
+                                    cap * sizeof(struct _GlobCtxNode));
+                        if (stack == NULL) {
+                            Mem_free(n.pattern);
+                            goto fail;
+                        }
+                        ctx->stack = stack;
+                        ctx->stack_capacity = cap;
+                    }
+                    uint32_t s_len = wcslen(data.cFileName);
+                    uint32_t tot_len = s_len + ctx->last_segment + rem_len + 1;
+                    wchar_t* s = Mem_alloc(tot_len * sizeof(wchar_t));
+                    if (s == NULL) {
+                        Mem_free(n.pattern);
+                        goto fail;
+                    }
+                    memcpy(s, n.pattern, ctx->last_segment * sizeof(wchar_t));
+                    memcpy(s + ctx->last_segment, data.cFileName, s_len * sizeof(wchar_t));
+                    memcpy(s + ctx->last_segment + s_len, n.pattern + ix, (rem_len + 1) * sizeof(wchar_t));
+                    s[ctx->last_segment + s_len] = L'\\';
+                    ctx->stack[ctx->stack_size].pattern = s;
+                    ctx->stack[ctx->stack_size].pattern_offset = ctx->last_segment + s_len + 1;
+                    ++ctx->stack_size;
+                } while (FindNextFileW(h, &data));
+                Mem_free(n.pattern);
+                break;
+            } else if (n.pattern[ix] == L'\0') {
+                if (has_glob) {
+                    HANDLE h = FindFirstFileW(n.pattern, &data);
+                    if (h == INVALID_HANDLE_VALUE) {
+                        --ctx->stack_size;
+                        Mem_free(n.pattern);
+                        break;
+                    }
+                    ctx->handle = h;
+                    if (matches(n.pattern + ctx->last_segment, data.cFileName)) {
+                        if (!WString_append_count(&p->path, n.pattern, ctx->last_segment)) {
+                            goto fail;
+                        }
+                        if (!WString_extend(&p->path, data.cFileName)) {
+                            goto fail;
+                        }
+                        *path = p;
+                        return true;
+                    }
+                } else if (GetFileAttributesW(n.pattern) != INVALID_FILE_ATTRIBUTES) {
+                    if (!WString_extend(&p->path, n.pattern)) {
+                        goto fail;
+                    }
+                    --ctx->stack_size;
+                    Mem_free(n.pattern);
+                    *path = p;
+                    return true;
+                } else {
+                    --ctx->stack_size;
+                    Mem_free(n.pattern);
+                }
+                break;
+            }
+        }
+    }
+
+fail:
+    if (ctx->handle != INVALID_HANDLE_VALUE) {
+        FindClose(ctx->handle);
+    }
+    WString_free(&ctx->p.path);
+    for (uint32_t ix = 0; ix < ctx->stack_size; ++ix) {
+        Mem_free(ctx->stack[ix].pattern);
+    }
+    ctx->stack_size = 0;
+    Mem_free(ctx->stack);
+    ctx->stack = NULL;
+    return false;
+}
+
+void Glob_abort(GlobCtx* ctx) {
+    if (ctx->stack == NULL) {
+        return;
+    }
+    if (ctx->handle != INVALID_HANDLE_VALUE) {
+        FindClose(ctx->handle);
+    }
+    WString_free(&ctx->p.path);
+    for (uint32_t ix = 0; ix < ctx->stack_size; ++ix) {
+        Mem_free(ctx->stack[ix].pattern);
+    }
+    ctx->stack_size = 0;
+    Mem_free(ctx->stack);
+    ctx->stack = NULL;
+}
+
 bool WalkDir_begin(WalkCtx* ctx, const wchar_t* dir, bool absolute_path) {
     WString* s = &ctx->p.path;
     ctx->absolute_path = absolute_path;
--- workdir/src/test.c
+++ workdir/src/test.c
@@ -2,239 +2,27 @@
 #define UNICODE
 #endif
 
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 #include "args.h"
 #include "glob.h"
-#include "json.h"
-#include "match_node.h"
 #include "printf.h"
-#include "subprocess.h"
-#include "path_utils.h"
-#include "cli.h"
 
-bool add_node(NodeBuilder *builder, const char *key, HashMap *extr_map,
-              MatchNode *node, WString *workbuf) {
-    if (strcmp(key, "&FILE") == 0) {
-        NodeBuilder_add_files(builder, true, node);
-    } else if (strcmp(key, "&FILELIKE") == 0) {
-        NodeBuilder_add_files(builder, false, node);
-    } else if (strcmp(key, "&DEFAULT") == 0) {
-        NodeBuilder_add_any(builder, node);
-    } else if (key[0] == '&') {
-        DynamicMatch *dyn = HashMap_Value(extr_map, key);
-        if (dyn != NULL) {
-            NodeBuilder_add_dynamic(builder, dyn, node);
-        } else {
-            _wprintf(L"Invalid extern \"%S\"\n", key);
-            return false;
-        }
-    } else {
-        if (!WString_from_utf8_str(workbuf, key)) {
-            _wprintf(L"Invalid static \"%S\"\n", key);
-            return false;
-        }
-        NodeBuilder_add_fixed(builder, workbuf->buffer, workbuf->length, node);
-    }
-    return true;
-}
-
-
-// replace all instances of $
-void substitute_commands(wchar_t *str, DWORD *len, DWORD capacity) {
-    wchar_t *pos = str;
-    if (*len == 0) {
-        return;
-    }
-    String outbuf;
-    String_create(&outbuf);
-    WString wbuf;
-    WString_create(&wbuf);
-
-    wchar_t last = str[*len - 1];
-    wchar_t end_par = 0;
-    if (last == L')' || last == L'}' || last == L']') {
-        end_par = last;
-    }
-    str[*len - 1] = L'\0';
-    while (pos < str + *len) {
-        wchar_t *dlr = wcschr(pos, L'$');
-        if (dlr == NULL) {
-            break;
-        }
-        pos = dlr + 1;
-        wchar_t par = *pos;
-        if (par == L'(') {
-            par = L')';
-        } else if (par == L'{') {
-            par = L'}';
-        } else if (par == L'[') {
-            par = L']';
-        } else {
-            continue;
-        }
-        wchar_t *close = wcschr(pos, par);
-        bool at_end = false;
-        if (close == NULL) {
-            if (end_par == par) {
-                at_end = true;
-                close = str + *len - 1;
-            } else {
-                break;
-            }
-        }
-        DWORD cmd_len = close - pos + 2;
-        *close = L'\0';
-        String_clear(&outbuf);
-        WString_clear(&wbuf);
-        unsigned long errorcode;
-        if (!subprocess_run(pos + 1, &outbuf, 5000, &errorcode, SUBPROCESS_STDIN_DEVNULL)) {
-            String_clear(&outbuf);
-        }
-        if (outbuf.length > 0 &&
-            !WString_from_utf8_bytes(&wbuf, outbuf.buffer, outbuf.length)) {
-            _wprintf(L"Failed decoding to utf-16\n");
-            WString_clear(&wbuf);
-        }
-        if ((*len + wbuf.length - cmd_len < capacity)) {
-            WString_replaceall(&wbuf, L'\n', ' ');
-            WString_replaceall(&wbuf, L'\r', ' ');
-            WString_replaceall(&wbuf, L'\t', ' ');
-            DWORD start = (pos - 1) - str;
-            DWORD old_end = 1 + close - str;
-            DWORD rem = *len - old_end;
-            DWORD new_end = start + wbuf.length;
-            memmove(str + new_end, str + old_end, rem * sizeof(wchar_t));
-            memcpy(str + start, wbuf.buffer, wbuf.length * sizeof(wchar_t));
-            *len = *len - cmd_len + wbuf.length;
-            if (at_end) {
-                last = str[*len - 1];
-                break;
-            }
-            close = str + new_end - 1;
-        } else {
-            if (at_end) {
-                break;
-            }
-            *close = par;
-        }
-        pos = close + 1;
-    }
-    str[*len - 1] = last;
-    String_free(&outbuf);
-    WString_free(&wbuf);
-}
-
-
-WString pathext, pathbuf, progbuf, workdir;
-
-const wchar_t* find_program(const wchar_t* cmd, WString* dest) {
-    unsigned ix = 0;
-    bool in_quote = false;
-    WString prog;
-    WString_clear(dest);
-    WString_clear(&progbuf);
-    while (1) {
-        if (cmd[ix] == L'\0' || (cmd[ix] == L' ' && !in_quote)) {
-            break;
-        }
-        if (cmd[ix] == L'"') {
-            in_quote = !in_quote;
-        } else {
-            WString_append(&progbuf, cmd[ix]);
-        }
-        ++ix;
-    }
-
-    if (!get_envvar(L"PATHEXT", 0, &pathext) || pathext.length == 0) {
-        WString_clear(&pathext);
-        WString_extend(&pathext, L".EXE;.BAT");
-    }
-    wchar_t *ext = pathext.buffer;
-    DWORD count = 0;
-    do {
-        wchar_t* sep = wcschr(ext, L';');
-        if (sep == NULL) {
-            sep = pathext.buffer + pathext.length;
-        }
-        *sep = L'\0';
-        do {
-            if (!WString_reserve(dest, count)) {
-                return NULL;
-            }
-            count = SearchPathW(workdir.buffer,
-                                progbuf.buffer, ext, dest->capacity,
-                                dest->buffer, NULL);
-        } while (count > dest->capacity);
-        dest->length = count;
-        dest->buffer[dest->length] = L'\0';
-        if (count > 0) {
-            return dest->buffer;
-        }
-        *sep = L';';
-        ext = sep + 1;
-    } while (ext < pathext.buffer + pathext.length);
-
-    if (!get_envvar(L"PATH", 0, &pathbuf)) {
-        return NULL;
-    }
-    ext = pathext.buffer;
-    count = 0;
-    do {
-        wchar_t* sep = wcschr(ext, L';');
-        if (sep == NULL) {
-            sep = pathext.buffer + pathext.length;
-        }
-        *sep = L'\0';
-        do {
-            if (!WString_reserve(dest, count)) {
-                return NULL;
-            }
-            count = SearchPathW(pathbuf.buffer,
-                                progbuf.buffer, ext, dest->capacity,
-                                dest->buffer, NULL);
-        } while (count > dest->capacity);
-        dest->length = count;
-        dest->buffer[dest->length] = L'\0';
-        if (count > 0) {
-            return dest->buffer;
-        }
-        *sep = L';';
-        ext = sep + 1;
-    } while (ext < pathext.buffer + pathext.length);
-    return NULL;
-}
-
-wchar_t* entries[] = {
-    L"This", L"Is", L"A", L"long test"
-};
 
 int main() {
-    wchar_t buf[1025];
-    memcpy(buf, L"Hello world", 11 * sizeof(wchar_t));
-    DWORD len = 11;
+    GlobCtx ctx;
+    Path* path;
+    const wchar_t* args = GetCommandLineW();
+    int argc;
+    wchar_t** argv = parse_command_line(args, &argc);
 
-    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
-    _wprintf(L"This custom prompt >");
-    CONSOLE_SCREEN_BUFFER_INFO cinfo;
-    GetConsoleScreenBufferInfo(out, &cinfo);
-    CHAR_INFO charBuf[256];
-    COORD bufSize;
-    bufSize.X = 256;
-    bufSize.Y = 1;
-    COORD bufCorner;
-    bufCorner.X = 0;
-    bufCorner.Y = 0;
-    SMALL_RECT area;
-    area.Top = area.Bottom = cinfo.dwCursorPosition.Y;
-    area.Left = 0;
-    area.Right = cinfo.dwCursorPosition.X;
-
-    ReadConsoleOutputW(out, charBuf, bufSize, bufCorner, &area);
-    Cli_Search(buf, &len, 1024, entries, 4);
-    buf[len] = L'\0';
-    bufSize.X = cinfo.dwCursorPosition.X;
-    WriteConsoleOutputW(out, charBuf, bufSize, bufCorner, &area);
-    SetConsoleCursorPosition(out, cinfo.dwCursorPosition);
-    _wprintf(L"Res: %s\n", buf);
+    for (uint32_t ix = 1; ix < argc; ++ix) {
+        _wprintf(L"Glob %s:\n", argv[ix]);
+        Glob_begin(argv[ix], &ctx);
+        while (Glob_next(&ctx, &path)) {
+            _wprintf(L"%s\n", path->path.buffer);
+        }
+    }
 
     return 0;
 }
